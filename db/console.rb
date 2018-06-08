74c59b856562d8833565f408f0c9ee1c46a578a4
jamos90 <jamos_98@hotmail.com>
2018-06-08 12:41:18 +0100

    film update done

--- db/console.rb
+++ db/console.rb
@@ -4,7 +4,8 @@ require_relative("../models/film.rb")
 require_relative("../models/ticket.rb")
 require_relative("sql_runner.rb")
 
-
+Customer.delete()
+Film.delete()
 
  customer1 = Customer.new({
    'name' => 'James',
@@ -19,6 +20,35 @@ require_relative("sql_runner.rb")
      })
 
      customer2.save()
+
+  film1 = Film.new({
+    'title' => 'Solo',
+    'price' => 13
+    })
+
+  film1.save()
+
+  film2 = Film.new({
+    'title' => 'Infinity War',
+    'price' => 20
+    })
+
+    film2.save()
+
+    ticket1 = Ticket.new({
+      'customer_id' => customer1.id,
+      'film_id' => film1.id
+      })
+
+      ticket1.save()
+
+    ticket2 = Ticket.new({
+      'customer_id' => customer2.id,
+      'film_id' => film2.id
+      })
+
+    ticket2.save()
+
 
 
   binding.pry
